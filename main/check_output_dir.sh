14d3508bea9ac776f964054b35ee747a62c28dc3
MrDreaming <729637403@qq.com>
2017-12-21 16:15:26 +0800

    delete the useless code

--- main/check_output_dir.sh
+++ main/check_output_dir.sh
@@ -1,15 +1,5 @@
 
-#echo "hello1"
-
 OUTPUT_PATH_X=../output/
-
-#$OUTPUT_PATH_X  =  "../output/"
-
-#echo ${OUTPUT_PATH_X};
-
-#$a = "just in case";
-
-#echo "take the gun,$a";
 
 echo "checking exist or not : " ${OUTPUT_PATH_X};
 
@@ -20,12 +10,3 @@ else
 	echo "exist : " ${OUTPUT_PATH_X};
 fi
 
-#if [ ! -f /data/wpa_supplicant.conf ]; then
-#if [  !  -d  ../output/  ]; then
-#if [  !  -d  ${OUTPUT_PATH_X} ]; then
-#	echo "hello2"
-#	mkdir ${OUTPUT_PATH_X}
-#	#cp /etc/wpa_supplicant.conf /data/wpa_supplicant.conf
-#fi
-
-#echo "hello3"
